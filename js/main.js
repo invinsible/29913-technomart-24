0a7475fe2afe0235b423c9e0cf25ad26b33359ce
Igor Sergeev <igor@tomatstudio.ru>
2019-02-28 14:11:20 +0300

    Стилизация формы обратной связи, анимация, js для popup

--- js/main.js
+++ js/main.js
@@ -1,39 +1,66 @@
 var mapLink = document.querySelector(".map-link");
 var formLink = document.querySelector(".callback-link");
+
 var popupMap = document.querySelector(".popup-map");
 var popupForm = document.querySelector(".popup-callback");
-var popup = document.querySelector(".popup");
-var popupClose = document.querySelector(".popup-close");
 
+//var popup = document.querySelectorAll(".popup");
+var popupClose = document.querySelectorAll(".popup-close");
+
+
+var toggleShow = function(arg) {
+  arg.classList.toggle("popup-show");
+};
 
 mapLink.addEventListener("click", function(evt) {
   evt.preventDefault();  
-  if (popupMap.classList.contains("popup-show")) {
-    popupMap.classList.remove("popup-show");
-  } else {
-    popupMap.classList.add("popup-show");
-  }  
+  toggleShow(popupMap);
 });
 
 formLink.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  if (popupForm.classList.contains("popup-show")) {
-    popupForm.classList.remove("popup-show");
-  } else {
-    popupForm.classList.add("popup-show");
-  }  
+  evt.preventDefault();  
+  toggleShow(popupForm);
 });
 
-popupClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.remove("popup-show");
-})
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+for (var i = 0, l = popupClose.length; i < l; i++) {
+  popupClose[i].addEventListener("click", function(evt) {
     evt.preventDefault();
-    if (popup.classList.contains("popup-show")) {
-      popup.classList.remove("popup-show");
-    }
-  }
-});
+    this.parentNode.classList.remove("popup-show");      
+  });
+} 
+
+
+
+
+
+// for (var c = 0, b = popupClose.length; c <= b; c++) {
+//   popupClose[c].addEventListener("click", function(evt) {    
+//   });
+
+
+
+
+
+// formLink.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   if (popupForm.classList.contains("popup-show")) {
+//     popupForm.classList.remove("popup-show");
+//   } else {
+//     popupForm.classList.add("popup-show");
+//   }  
+// });
+
+// for (var i = 0, l = popupClose.length; i <= l; i++) {
+//   popupClose[i].addEventListener("click", function(evt) {    
+//   });
+// }
+
+// window.addEventListener("keydown", function (evt) {
+//   if (evt.keyCode === 27) {
+//     evt.preventDefault();
+//     if (popup.classList.contains("popup-show")) {
+//       popup.classList.remove("popup-show");
+//     }
+//   }
+// });

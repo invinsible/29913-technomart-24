c12d26bf3275450f73ccba1377d385892e0f8286
Igor Sergeev <igor@tomatstudio.ru>
2019-03-06 11:32:17 +0300

    Автопрефикс и минификация файлов

--- js/main.js
+++ js/main.js
@@ -41,35 +41,3 @@ for (var i = 0, l = itemBuy.length; i < l; i++) {
     popupBasket.classList.add("popup-show");      
   });
 }
-
-
-// for (var c = 0, b = popupClose.length; c <= b; c++) {
-//   popupClose[c].addEventListener("click", function(evt) {    
-//   });
-
-
-
-
-
-// formLink.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   if (popupForm.classList.contains("popup-show")) {
-//     popupForm.classList.remove("popup-show");
-//   } else {
-//     popupForm.classList.add("popup-show");
-//   }  
-// });
-
-// for (var i = 0, l = popupClose.length; i <= l; i++) {
-//   popupClose[i].addEventListener("click", function(evt) {    
-//   });
-// }
-
-// window.addEventListener("keydown", function (evt) {
-//   if (evt.keyCode === 27) {
-//     evt.preventDefault();
-//     if (popup.classList.contains("popup-show")) {
-//       popup.classList.remove("popup-show");
-//     }
-//   }
-// });
